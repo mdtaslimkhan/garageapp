e60eaba68dd3df5fda74fda52698d75315da34d5
mdtaslimkhan <asalam773@gmail.com>
2025-08-08 10:18:40 +0600

    route added

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@
 Auth::routes();
 
 Route::get('/foo', function () {
-   $targetFolder = $_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
-        $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/public/storage';
-        symlink($targetFolder,$linkFolder);
+    $targetFolder = $_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
+	$linkFolder = $_SERVER['DOCUMENT_ROOT'].'/public/storage';
+	symlink($targetFolder,$linkFolder);
         echo 'Symlink process successfully completed';
 });
 
